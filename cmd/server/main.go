2ca1b7e8fde799c644fed43156ac2dda07c49252
Yavor  Shahpasov <yavosh@gmail.com>
2021-02-12 12:21:20 +0200

    Add lb

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+)
+
+var (
+	hostname string
 )
 
 func main() {
 	listenAddress := ":8080"
+	hostname, _ = os.Hostname()
+
 	log.Printf("Starting server @ %s", listenAddress)
 	http.HandleFunc("/", logger(handler))
 	log.Panic(http.ListenAndServe(listenAddress, nil))
@@ -15,8 +22,8 @@ func main() {
 
 func logger(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.String())
-		f(w,r)
+		log.Printf("%s %s %s", hostname, r.Method, r.URL.String())
+		f(w, r)
 	}
 }
 
@@ -24,7 +31,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(http.StatusOK)
-	_, err := fmt.Fprintf(w, "Hello from "+r.URL.String())
+
+	_, err := fmt.Fprintf(w, "Hello from %s -> %s %s",
+		hostname, r.Method, r.URL.String())
 	if err != nil {
 		log.Printf("Error %v", err)
 	}
--- ops/scripts/build-images.sh
+++ ops/scripts/build-images.sh
@@ -2,3 +2,7 @@
 
 docker buildx build -t "yavosh/k8s-test:latest" \
   --platform linux/amd64,linux/arm64,linux/arm/v7 --push .
+
+docker buildx build -t "yavosh/k8s-test-job:latest" \
+  -f ./Dockerfile-job \
+  --platform linux/amd64,linux/arm64,linux/arm/v7 --push .
